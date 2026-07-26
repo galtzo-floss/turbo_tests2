5758f2a3feac6921e8412fb98ca75730201fcf67
Peter H. Boling <peter.boling@gmail.com>
2026-07-26 02:31:01 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- lib/turbo_tests/version.rb
+++ lib/turbo_tests/version.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 module TurboTests
+  # Version namespace for this gem.
   module Version
+    # Current gem version.
     VERSION = "3.2.2"
   end
+  # Current gem version exposed at the traditional constant location.
   VERSION = Version::VERSION # Traditional Constant Location
 end
